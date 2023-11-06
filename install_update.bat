8908ed775f14866d69075ad2d5e70f6a4490eb1d
LeemONadDev <hackermix101@gmail.com>
2023-11-06 23:52:11 +0200

    Create

---
+++ install_update.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing and upgrading: requests
+pip install requests --upgrade
+py.exe pip install requests --upgrade
+python.exe pip install requests --upgrade
+echo Updated succesfully!
+pause
--- simple_file_db.py
+++ simple_file_db.py
@@ -1,43 +1,43 @@
 import os
 def get_float_value_from_db(filename):
-    filename = "bot_database\\"+filename
+    filename = "db\\"+filename
     f = open(filename,"r",encoding="utf-8")
     float_value_result = float(f.read())
     f.close()
     return float_value_result
 
 def write_float_value_to_db(filename,value):
-    filename = "bot_database\\"+filename
+    filename = "db\\"+filename
     f = open(filename,"w",encoding="utf-8")
     f.write(str(value))
     f.close()
 
 def create_db(filename):
-    filename = "bot_database\\"+filename
+    filename = "db\\"+filename
     f = open(filename,"x",encoding="utf-8")
     f.close()
 
 def write_string_to_db(filename,string,end="\n"):
-    filename = "bot_database\\"+filename
+    filename = "db\\"+filename
     string = string + end
     f = open(filename,'a+',encoding="utf-8")
     f.write(string)
     f.close()
 
 def change_string_in_db(filename,string):
-    filename = "bot_database\\"+filename
+    filename = "db\\"+filename
     f = open(filename,"w",encoding="utf-8")
     f.write(string)
     f.close()
 
 def get_string_value_from_db(filename):
-    filename = "bot_database\\"+filename
+    filename = "db\\"+filename
     f = open(filename,"r",encoding="utf-8")
     string_value_result = f.read()
     f.close()
     return string_value_result
 
 def make_db_dir(filename):
-    filename = "bot_database\\"+filename
+    filename = filename
     temp_command_data_tmp = "mkdir "+filename
     os.system(temp_command_data_tmp)
